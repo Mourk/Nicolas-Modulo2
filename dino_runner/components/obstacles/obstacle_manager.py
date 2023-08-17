ba262fa015d80ae75702cc44d03fc3d80f845baf
Nicullas <46252119+Mourk@users.noreply.github.com>
2023-08-17 09:06:45 -0300

    git commit -m "meucommit"

---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,31 @@
+import random
+import pygame
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.components.obstacles.cactus import Cactus
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+
+    def update(self, game):
+        if not self.obstacles:
+            self.obstacles.append(random.choice([Cactus(), Bird()]))
+
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
+            
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                if not game.player.has_power_up:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
+
+    def reset_obstacles(self):
+        self.obstacles = []
+
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
